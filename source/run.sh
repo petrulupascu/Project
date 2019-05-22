2fc051d102d706140c87f48a9ae85fe31ea162f9
Petru <pvlupascu@gmail.com>
2019-05-22 14:19:33 +0200

    fixed bibliography issue

--- source/run.sh
+++ source/run.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
 BASE="${1%.*}"
 pdflatex $BASE.tex
 if [ $? -ne 0 ]; then
     echo "Compilation error. Check log."
     exit 1
 fi
-bibtex $BASE
+biber $BASE
 pdflatex $BASE.tex
 pdflatex $BASE.tex
 exit 0
